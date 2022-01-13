55327bfc4da3a0bd8728137121821d02041dc505
Long Hồ <thienlong460@gmail.com>
2022-01-13 17:10:11 +0700

    + get all posts in 3 month ago

--- src/controllers/influencers/updateInfluencerController.js
+++ src/controllers/influencers/updateInfluencerController.js
@@ -3,6 +3,7 @@ import { updateInfluencer } from '../../services/influencers';
 import { PAGE_ID } from '../../constants';
 const FB = require('fb');
 import axios from 'axios';
+import moment from 'moment';
 
 // This is where a route is handled, the function MUST accept 2 params request and response.
 // Request will include all the information of the INCOMING request
@@ -79,6 +80,24 @@ export default async (req, res) => {
 	return res.status(200).json(influencer);
 };
 
+// async function updateToken(client_id, client_secret, refresh_token) {
+// 	FB.api('oauth/access_token', {
+// 		client_id: client_id,
+// 		client_secret: client_secret,
+// 		grant_type: 'fb_exchange_token',
+// 		fb_exchange_token: refresh_token
+// 	}, function (res) {
+// 		if(!res || res.error) {
+// 			console.log(!res ? 'error occurred' : res.error);
+// 			return;
+// 		}
+
+// 		var accessToken = res.access_token;
+// 		var expires = res.expires ? res.expires : 0;
+// 		return accessToken;
+// 	});
+// }
+
 // name, link, followers_count, category_list
 async function get_basic_info(access_token) {
 	try {
@@ -121,9 +140,22 @@ async function get_categories(access_token) {
 
 	var posts = [];
 	for (let i = 0; i < res1.data.length; i++) {
-		if (res1.data[i].message !== undefined) posts.push(res1.data[i].message);
+		if (res1.data[i].message !== undefined) {
+			var threeMonthAgo = new Date(
+				new Date().getFullYear(),
+				new Date().getMonth() - 3,
+				new Date().getDate()
+			);
+
+			// console.log(threeMonthAgo - new Date(res1.data[i].created_time));
+
+			// get posts in 3 months ago
+			if (threeMonthAgo - new Date(res1.data[i].created_time) <= 0)
+				posts.push(res1.data[i].message);
+		}
 	}
 
+	// console.log('posts');
 	// console.log(posts);
 
 	const res2 = await axios({
@@ -132,6 +164,7 @@ async function get_categories(access_token) {
 		data: { texts: posts },
 	});
 
+	console.log('categories');
 	console.log(res2.data.categories);
 
 	return { categories: res2.data.categories };
